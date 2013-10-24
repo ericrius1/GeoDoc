fe112ac82d6954de95f9aa3f0b9cb2a8e636e6ea
ericrius1 <ericrius1@gmail.com>
2013-10-24 13:39:54 -0700

    changing controls

--- js/emitter.js
+++ js/emitter.js
@@ -2,11 +2,12 @@ var Emitter = function(lat, lon, numUsers, locationsGroup) {
 
 
   var emitter;
-  var maxAge = 2;
-  var particlesPerSecond = 100;
+  var particlesPerSecond = 10;
   var lat = lat;
   var lon = lon;
   var numUsers = numUsers;
+
+  var velFactor = 3;
 
 
   var xPos, yPos, zPos;
@@ -17,9 +18,6 @@ var Emitter = function(lat, lon, numUsers, locationsGroup) {
 
   var init = function() {
 
-  
-
-
     var phi = (90 - lat) * Math.PI / 180;
     var theta = (180 - lon) * Math.PI / 180;
 
@@ -27,15 +25,12 @@ var Emitter = function(lat, lon, numUsers, locationsGroup) {
     yPos =  Math.cos(phi);
     zPos =  Math.sin(phi) * Math.sin(theta);
 
-
     emitter = new ShaderParticleEmitter({
-      position: new THREE.Vector3(xPos/10, yPos/10, zPos/10),
+      position: new THREE.Vector3(xPos * 0.1, yPos * 0.1, zPos * 0.1),
 
-      acceleration: new THREE.Vector3(0, 0, 0),
-      accelerationSpread: new THREE.Vector3(.05, 0.01, .05),
 
-      velocity: new THREE.Vector3(xPos, yPos, zPos),
-      velocitySpread: new THREE.Vector3(.01, 0.075, .01),
+      velocity: new THREE.Vector3(xPos/velFactor, yPos/velFactor, zPos/velFactor),
+      velocitySpread: new THREE.Vector3(.01, 0.01, .01),
 
       colorStart: new THREE.Color('white'),
       colorEnd: new THREE.Color('blue'),
