90a4b8b91e006de70ee5e7513c1ec671f93e31ee
ericrius1 <ericrius1@gmail.com>
2013-10-24 12:35:48 -0700

    emmitters refactor

--- js/Emitters.js
+++ js/Emitters.js
@@ -1,8 +1,23 @@
-var Emitters = function(){
+var Emitters = function(locationData, scene){
 
-	function init(locations){
+	var scene = scene;
+	var locationData = locationData;
+	var emitters = [];
 
+	var init = function(){
+		emitters.push(new Emitter(scene));
+		emitters[emitters.length-1].init()
 
 	}
+
+	var tick = function(dt){
+		for(var i = 0; i < emitters.length; i++){
+			emitters[i].tick(dt)
+		}
+
+	}
+
+	this.init = init;
+	this.tick = tick;
 
 }
