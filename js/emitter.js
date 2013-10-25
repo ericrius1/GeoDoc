aa64c875c1386c01d6ea220734b88047a8ef0248
ericrius1 <ericrius1@gmail.com>
2013-10-24 17:06:35 -0700

    mapping color to numUsers

--- js/emitter.js
+++ js/emitter.js
@@ -5,9 +5,11 @@ var Emitter = function(lat, lon, numUsers, locationsGroup) {
   var particlesPerSecond = 20;
   var lat = lat;
   var lon = lon;
-  var numUsers = 1000;
+  var numUsers = randomRange(100, 1000)
+  console.log('numUsers', numUsers)
+  var color;
 
-  var velMultiplier = (Math.random() * 1000 )/1000;
+  var velMultiplier = numUsers / 1000;
 
   var surfFactor = 0.5;
 
@@ -24,10 +26,10 @@ var Emitter = function(lat, lon, numUsers, locationsGroup) {
     var phi = (90 - lat) * Math.PI / 180;
     var theta = (180 - lon) * Math.PI / 180;
 
-    xPos =  Math.sin(phi) * Math.cos(theta);
-    yPos =  Math.cos(phi);
-    zPos =  Math.sin(phi) * Math.sin(theta);
-
+    xPos = Math.sin(phi) * Math.cos(theta);
+    yPos = Math.cos(phi);
+    zPos = Math.sin(phi) * Math.sin(theta);
+    color = mapColor(xPos);
     emitter = new ShaderParticleEmitter({
       position: new THREE.Vector3(xPos * surfFactor, yPos * surfFactor, zPos * surfFactor),
 
@@ -35,11 +37,11 @@ var Emitter = function(lat, lon, numUsers, locationsGroup) {
       velocity: new THREE.Vector3(xPos * velMultiplier, yPos * velMultiplier, zPos * velMultiplier),
       velocitySpread: new THREE.Vector3(.1, 0.1, .1),
 
-      accelertion: new THREE.Vector3(1,1,1),
+      accelertion: new THREE.Vector3(1, 1, 1),
 
 
-      colorStart: new THREE.Color('white'),
-      colorEnd: new THREE.Color('blue'),
+      colorStart: color,
+      colorEnd: color,
       size: .2,
       sizeEnd: .01,
 
@@ -56,6 +58,13 @@ var Emitter = function(lat, lon, numUsers, locationsGroup) {
 
   var tick = function(dt) {
     locationsGroup.tick(dt);
+  }
+
+  var mapColor = function(x) {
+    var c = new THREE.Color();
+    // h,s,l ranges are in 0.0 - 1.0
+    c.setHSL(x,  .5, .5);
+    return c;
   }
   this.init = init;
   this.tick = tick;
--- js/utils.js
+++ js/utils.js
@@ -1,4 +1,8 @@
-var randomRange = function(){
+var randomRange = function(min, max) {
 
-	 return Math.random() * (max - min) + min;
+	return Math.random() * (max - min) + min;
+}
+
+var map = function(value, min1, max1, min2, max2) {
+	return min2 + (max2 - min2) * ((value - min1) / (max1 - min1));
 }
